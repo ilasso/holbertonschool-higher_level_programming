5626aba5c2facc609b4a5076395bcc78b95753de
ilasso <1204@holbertonschool.com>
2019-12-13 02:35:21 +0000

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    size = len(sys.argv) - 1
+    if size != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if sys.argv[2] not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    elif sys.argv[2] == '+':
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sys.argv[2] == '-':
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == '/':
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
