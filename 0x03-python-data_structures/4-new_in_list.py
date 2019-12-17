404aabbeb69c59f81735e566604e18a5e5c9bbd0
ilasso <1204@holbertonschool.com>
2019-12-17 23:32:44 +0000

    4-new_in_list.py mistake

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,6 +7,4 @@ def new_in_list(my_list, idx, element):
             for i in my_list:
                 new_my_list.append(i)
             new_my_list[idx] = element
-            return new_my_list
-        else:
-            return None
+        return new_my_list
