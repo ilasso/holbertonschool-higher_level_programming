4453584f908fc6f79f290068f90dd09a3d77c452
ilasso <1204@holbertonschool.com>
2019-12-18 04:16:56 +0000

    8-multiple_returns.py sentece empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
+    if length == 0:
+        return (length, None)
     first = sentence[0]
     return (length, first)
