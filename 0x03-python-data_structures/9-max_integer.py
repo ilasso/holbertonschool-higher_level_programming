084283ceea32555d785046a402b7d4a5978aa426
ilasso <1204@holbertonschool.com>
2019-12-18 04:41:30 +0000

    9-max_integer.py 2

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    maxim = 0
+    maxim = my_list[0]
     for number in my_list:
         if number > maxim:
             maxim = number
