18d74c30909c594c76cc8aac241938a9e4398434
ilasso <1204@holbertonschool.com>
2019-12-18 03:14:11 +0000

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix:
+        for i in matrix:
+            sizerow = len(i) - 1
+            k = 0
+            for j in i:
+                if k == sizerow:
+                    print("{:d}".format(j))
+                else:
+                    print("{:d}".format(j), end=" ")
+                k += 1
