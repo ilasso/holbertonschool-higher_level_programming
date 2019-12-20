ebd101108795b2e4fac899eeef0644efea59826f
ilasso <1204@holbertonschool.com>
2019-12-20 04:42:13 +0000

    12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    romandic = dict([('I', 1), ('V', 5), ('X', 10), ('L', 50),
+                     ('C', 100), ('D', 500), ('M', 1000)])
+    number = 0
+    size = len(roman_string) - 1
+    for i, j in enumerate(roman_string):
+        number += romandic.get(j)
+    if romandic.get(roman_string[i]) > romandic.get(roman_string[i-1]):
+        number -= (romandic.get(roman_string[i]) +
+                   romandic.get(roman_string[i-1]))
+        number += (romandic.get(roman_string[i]) -
+                   romandic.get(roman_string[i-1]))
+    return number
