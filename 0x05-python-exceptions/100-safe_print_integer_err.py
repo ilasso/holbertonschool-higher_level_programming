7fe4b6993e9d7e548589244634aae88954d5d81e
ilasso <1204@holbertonschool.com>
2020-01-08 18:14:15 +0000

    100-safe_print_integer_err.py pep8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as te:
-        print("Exception: {}".format(te))
+    except (ValueError, TypeError) as te:
+        print("Exception: {}".format(te), file=sys.stderr)
         return False
