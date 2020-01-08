9121a8af6aeaab29f5009eff8d9959e98a0e37bb
ilasso <1204@holbertonschool.com>
2020-01-08 17:56:13 +0000

    3-safe_print_division.py include valuerror

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,5 +4,7 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
+    except ValueError:
+        result = None
     finally:
         print("Inside result: {}".format(result))
