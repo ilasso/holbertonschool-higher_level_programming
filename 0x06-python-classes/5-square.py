1a300b6d114d489a2fb3145594fea08ac01ed99b
ilasso <1204@holbertonschool.com>
2020-01-10 04:09:45 +0000

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+   Name Module: 5-Square.
+   Description: Module that define Square Class with private atrribute (size)
+"""
+
+
+class Square():
+    """
+       Name Class: Square.
+       Description: define a private attribute
+    """
+    def __init__(self, size=0):
+        """
+           Name Function: __init__
+           Description:define and initialize private attribute
+                       with a value arg.
+                       if arg not integer raise typerro
+                       if arg < 0 raise value error
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+           Name Function: area
+           Description:define and initialize private attribute
+                       with a value arg.
+                       if arg not integer raise typerro
+                       if arg < 0 raise value error
+                       calculate square area and return value
+        """
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+           Name Function: size getter
+           Description: return object atribute size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+           Name Function: size setter
+           Description: modify object atribute size
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        """
+           Name Function: my_print
+           Description:prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            for j in range(0, self.__size):
+                print("#", end="")
+                if j == (self.__size - 1):
+                    print()
