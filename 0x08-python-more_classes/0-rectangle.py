cf0c39a4b30b87e4145c41d08d255b1a961ef442
ilasso <1204@holbertonschool.com>
2020-01-14 23:54:14 +0000

    task 0 doc

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+   Name Module: 0-Rectabgle
+   Description: Module that define Class Rectangle
+"""
+
+
 class Rectangle:
+    """
+       Name Class: Rectangle.
+       Description: define a Rectagle class
+    """
     pass
