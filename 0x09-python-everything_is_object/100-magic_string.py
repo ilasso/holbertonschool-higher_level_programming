6908a104ee41483d894ccc715b346683dff51eca
ilasso <1204@holbertonschool.com>
2020-01-15 23:44:39 +0000

    drop documentation

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
-"""
-   Name Module: 100-magic_string
-   Description: Module that returns a string “Holberton” n times
-                the number of the iteration
-"""
-
-
 def magic_string(lista=[]):
-    """
-      Name Function: magic_string
-      Description: Returns a string “Holberton” n times
-                   the number of the iteration
-    """
     lista += ["Holberton"]
     return ", ".join(lista)
