28e32c2decf6d7038e61b0cfc108b2f2de88d083
ilasso <1204@holbertonschool.com>
2020-01-15 00:17:12 +0000

    task2 perimeter error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -84,4 +84,6 @@ class Rectangle:
            Description:calculate perimeter of rectangle
            Return: instance rectangle perimeter
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width * 2) + (self.__height * 2)
