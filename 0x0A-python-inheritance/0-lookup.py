b30a9746ac7bea9930d230565dc36ac23cede65d
ilasso <1204@holbertonschool.com>
2020-01-20 23:08:19 +0000

    task 0 doc

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+   Name Module: 0-lookup.
+   Description: Module that returns the list of available attributes 
+                and methods of an object
+"""
 def lookup(ob):
+    """
+       Name Functions: Loockup.
+       Description: returns the list of available attributes and 
+                    methods of an object
+    """
     return dir(ob)
