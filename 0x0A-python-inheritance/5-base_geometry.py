ee135e0e020ed3324bac346becd9e2fa0ca9e2f0
ilasso <1204@holbertonschool.com>
2020-01-21 02:35:37 +0000

    task5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+   Name Module: 5-base_geometry
+   Description: Module that define a BaseGeometry empty class
+"""
+
+
+class BaseGeometry():
+    """
+       Name Class: BaseGeometry
+       Description: Empty class
+    """
+    pass
