644c9d611c25e0700ab5b78deb3fe9c94bfb58b0
ilasso <1204@holbertonschool.com>
2020-03-02 23:32:45 +0000

    top city2

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
--- displays the average temperature (Fahrenheit) by city ordered by temperature (descending) top 3, jul and ago.
-SELECT city, avg(value) avg_temp FROM temperatures where month in (7,8) GROUP BY city ORDER BY 2 DESC LIMIT 3;
+-- displays the ave tempby city ordered by avg(descending) top 3, jul and ago.
+SELECT city, avg(value) avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY 2 DESC LIMIT 3;
