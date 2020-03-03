15d426961972ed43043aa361b74d503e61c37634
ilasso <1204@holbertonschool.com>
2020-03-03 23:15:38 +0000

    8-cities_of_california_subquery.sql in

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- show cities of california
 -- not allowed to use the JOIN keyword
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
+SELECT * FROM cities WHERE state_id in (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
