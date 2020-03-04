386ede44127de2281b293f223139631b9d994533
ilasso <1204@holbertonschool.com>
2020-03-04 02:53:16 +0000

    13-count_shows_by_genre.sql genre name col

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 --  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each. 
 --  Each record should display: <TV Show genre> - <Number of shows linked to this genre>
-SELECT tv_genres.name, count(*) number_of_shows
+SELECT tv_genres.name genre, count(*) number_of_shows
 FROM tv_show_genres, tv_genres
 WHERE tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
