dfe5204d7d47bdc948c7cf74fe7a79cab08ad428
ilasso <1204@holbertonschool.com>
2020-03-28 02:29:16 +0000

    task1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+module:0-select_states.py
+description: use MySQLdb package to access db and display states rows
+             where name start with Upper 'N'
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cursor = db.cursor()
+    numrows = cursor.execute("SELECT * FROM states\
+                              WHERE name like 'N%' ORDER BY id ASC")
+    for i in cursor.fetchall():
+        print(i)
+    cursor.close()
+    db.close()
