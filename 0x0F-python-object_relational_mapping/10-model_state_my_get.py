6c00fd846ea19482dc42d96d1e7833af833975a0
ilasso <1204@holbertonschool.com>
2020-03-29 02:28:32 +0000

    10-model_state_my_get.py v4

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,6 +8,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import (create_engine)
 from model_state import State, Base
 
+
 if __name__ == "__main__":
 
     engine = create_engine(
@@ -15,10 +16,12 @@ if __name__ == "__main__":
                                                          argv[2],
                                                          argv[3]),
              pool_pre_ping=True)
+
     session = Session(engine)
     obj = session.query(State).filter(State.name == argv[4])\
                               .first()
     if obj is None:
-        print("Not Found")
+        print("Not found")
     else:
         print(obj.id)
+    session.close()
