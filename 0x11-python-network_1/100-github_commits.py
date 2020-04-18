23faced599031e8ae756af75d7c96c1a05d1bbda
ilasso <1204@holbertonschool.com>
2020-04-18 21:38:27 +0000

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+prints github repository information
+"""
+
+
+if __name__ == '__main__':
+    import sys
+    import requests
+
+    user = sys.argv[1]
+    repo = sys.argv[2]
+
+    url = "https://api.github.com/repos/{}/{}/commits".format(user, repo)
+
+    r = requests.get(url)
+    count = 0
+    for i in r.json():
+        count = count + 1
+        for j, k in i.items():
+            if j == 'commit':
+                sha = k['tree']['sha']
+                name = k['author']['name']
+                print("{}: {}".format(sha, name))
+                if count == 10:
+                    exit()
