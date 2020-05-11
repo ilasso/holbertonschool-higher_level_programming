7af7f16d52d3cd973aef1527433431c4f8d804bd
ilasso <1204@holbertonschool.com>
2020-05-11 23:50:33 +0000

    8-esrever.js

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let j = 0;
+  console.log(list.length);
+  for (let i = list.length - 1; i !== 0 && i > j; i--) {
+    let aux = list[j];
+    list[j] = list[i];
+    list[i] = aux;
+    aux = 0;
+    j++;
+  }
+  return list;
+};
