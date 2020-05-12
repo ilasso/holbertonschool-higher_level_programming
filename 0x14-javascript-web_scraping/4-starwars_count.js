f1c56678a9e60355590167940f9d9b9ab86a5298
ilasso <1204@holbertonschool.com>
2020-05-12 20:45:34 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,13 +5,15 @@ request(process.argv[2], function (error, response, body) {
     console.error(error);
   }
   const jsonbody = JSON.parse(body);
-  const chars = jsonbody.results[0].characters;
-
+  const chars = jsonbody.results;
+  let count = 0;
   for (let i = 0; i < chars.length; i++) {
-    if (chars[i].includes('/18/')) {
-      request(chars[i], function (error2, response2, body2) {
-        console.log(JSON.parse(body2).films.length);
-      });
+    const movies = chars[i].characters;
+    for (let j = 0; j < movies.length; j++) {
+      if (movies[j].includes('/18/')) {
+        count++;
+      }
     }
   }
+  console.log(count);
 });
