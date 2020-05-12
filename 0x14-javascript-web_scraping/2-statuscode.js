c12b61593a8a191e41ca3ebce131d6ca6027dc5d
ilasso <1204@holbertonschool.com>
2020-05-12 18:50:15 +0000

    2-statuscode.js 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.error('Code:', error);
+    console.error('code:', error);
   }
-  console.log('Code:', response && response.statusCode);
+  console.log('code:', response && response.statusCode);
 });
