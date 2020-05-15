574e1830763100642328524681d33f2ea23f3007
ilasso <1204@holbertonschool.com>
2020-05-15 06:32:07 +0000

    7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,6 @@
-let url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+// Write a Javascript script that fetches and replaces the name of this
+// URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
 $.get(url, function (data, stat) {
   $('div#character').text(data.name);
 });
