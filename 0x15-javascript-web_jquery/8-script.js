22433d4d82edcf0ce5891cfc0c04bc2e6acf22dd
ilasso <1204@holbertonschool.com>
2020-05-15 07:03:26 +0000

    8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,9 @@
-// script that fetches and lists all movies title by using this 
+// script that fetches and lists all movies title by using this
 // URL: https://swapi-api.hbtn.io/api/films/?format=json
-let url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 $.get(url, function (data) {
-  let films = data.results;
-  for (let film of films) {
+  const films = data.results;
+  for (const film of films) {
     $('ul#list_movies').append(`<li>${film.title}</li>`);
   }
 });
