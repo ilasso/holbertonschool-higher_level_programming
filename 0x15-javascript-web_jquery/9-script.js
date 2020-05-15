aed9961c97622c9cc822b9142416aa422eb1af81
ilasso <1204@holbertonschool.com>
2020-05-15 22:20:11 +0000

    9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,4 +1,7 @@
-let url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+// Write a Javascript script that fetches from https://fourtonfish.com/hellosalut/?lang=fr
+// and displays the value of hello from that fetch in the HTML’s tag DIV#hello
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
 $.get(url, function (data) {
-	$('div#hello').text(data.hello) }
+  $('div#hello').text(data.hello);
+}
 );
