11bfa7b0810e8c1620fc3cad38e1e407f0e91477
ilasso <55113983+ilasso@users.noreply.github.com>
2020-05-15 00:49:46 -0500

    jq:task6
    jq:task6

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,5 @@
+// script that updates the text of the HTML tag HEADER 
+// to “New Header!!!” when the user clicks on DIV#update_header
+$('div#update_header').click(function () {
+  $('header').text('New Header!!!');
+});
