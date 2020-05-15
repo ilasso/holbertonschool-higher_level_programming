a4d9dc267bceb989d9c1d878af1c003b22eb0f53
ilasso <1204@holbertonschool.com>
2020-05-15 04:09:03 +0000

    semistandard:task2

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
-// script that updates the text color of the HTML tag HEADER to red (#FF0000) 
+// script that updates the text color of the HTML tag HEADER to red (#FF0000)
 // when the user clicks on the tag DIV#red_header.
 $('div#red_header').click(function () {
   $('header').css('color', '#FF0000');
